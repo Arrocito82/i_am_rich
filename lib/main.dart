38f92cb360391be87fb1b1a18f0498ac79a1658e
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-03-27 12:58:47 -0600

    change image url to image file, plus title style

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,24 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.green[200],
+          title: Center(
+            child: Text(
+              'I Am Rich',
+              textScaleFactor: 1.5,
+              style: TextStyle(fontFamily: 'LibreBaskerville', fontStyle: FontStyle.italic),
+            ),
+            // heightFactor: 5,
+          ),
+          backgroundColor: Colors.pink[900],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage('https://picsum.photos/400/300'),
+            // image: NetworkImage('https://picsum.photos/400/300'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
-        backgroundColor: Colors.grey[50],
+        backgroundColor: Colors.grey[100],
       ),
     ),
   );
 }
-
-
